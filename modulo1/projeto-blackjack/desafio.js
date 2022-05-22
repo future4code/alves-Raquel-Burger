4baa6bcaddbf5b8112bd11d13fa22c88bff83c27
Raquel Baptista Burger <raquelburgerpsi@gmail.com>
2022-05-22 13:00:00 -0300

    Versão com desafio

--- modulo1/projeto-blackjack/desafio.js
+++ modulo1/projeto-blackjack/desafio.js
@@ -1,37 +1,57 @@
-console.log("Boas vindas ao jogo de Blackjack!")
 const jogadaUsuario = {
-   carta1 : comprarCarta(),
-   carta2 : comprarCarta(),
+   carta1: comprarCarta(),
+   carta2: comprarCarta(),
+   carta3: comprarCarta(),
+   carta4: comprarCarta()
 }
 const jogadaComputador = {
-   carta1 : comprarCarta(),
-   carta2 : comprarCarta(),
+   carta1: comprarCarta(),
+   carta2: comprarCarta()
 }
-const pontuacaoUsuario = jogadaUsuario.carta1.valor + jogadaUsuario.carta2.valor
-const pontuacaoComputador = jogadaComputador.carta1.valor + jogadaComputador.carta2.valor
+let pontuacaoUsuario = jogadaUsuario.carta1.valor + jogadaUsuario.carta2.valor
+let pontuacaoComputador = jogadaComputador.carta1.valor + jogadaComputador.carta2.valor
 
-const jogando = () => {
-   let inicio = true
-   inicio = confirm("Quer iniciar uma nova rodada?")
-   while(inicio !== false){
-      if((jogadaUsuario.carta1.valor === 11 && jogadaUsuario.carta2.valor == 11) || (jogadaComputador.carta1.valor == 11 && jogadaComputador.carta2.valor == 11)){
-         inicio = confirm("As cartas do usuário foram dois Ases, deseja retira as cartas novamente?")
-         
-      }else{
-         console.log(`Usuário - cartas: ${jogadaUsuario.carta1.texto} ${jogadaUsuario.carta2.texto} - pontuação ${pontuacaoUsuario}`)
-         console.log(`Computador - cartas: ${jogadaComputador.carta1.texto} ${jogadaComputador.carta2.texto} - pontuação ${pontuacaoComputador}`)
-         inicio = false 
-      }  
-   }
-   if(pontuacaoUsuario > pontuacaoComputador){
-      console.log(`O usuário ganhou!`)
-   }else if(pontuacaoUsuario < pontuacaoComputador){
-      console.log(`O computador ganhou!`)
+function calculaPontos (pontosUsuario, pontosComputador) {
+   if((pontosUsuario > pontosComputador) && (pontosUsuario <= 21)){
+      return `O usuário ganhou!`
+   }else if((pontosUsuario === pontosComputador)){
+      return `Empate!`
    }else{
-      console.log(`Empate!`)
-   }   
-   console.log("O jogo acabou.")
+      return `O computador ganhou!`
+   }
 }
 
-jogando()
+function jogando (pontos) {
+   let inicio = true
+   inicio = confirm(`Bem vindes ao jogo de Blackjack!\nQuer iniciar uma nova rodada?`)
+   
+   if(inicio === false){
+      alert("O jogo acabou!")
+   }else{
+      while((inicio !== false)){
+         if((jogadaUsuario.carta1.valor === 11 && jogadaUsuario.carta2.valor == 11) || (jogadaComputador.carta1.valor == 11 && jogadaComputador.carta2.valor == 11)){
+            inicio = confirm("As cartas retiradas foram dois Ases, deseja retira as cartas novamente?")
+         }else{
+            inicio = confirm(`Suas cartas são ${jogadaUsuario.carta1.texto} ${jogadaUsuario.carta2.texto}. A carta revelada do computador é ${jogadaComputador.carta1.texto}\nDeseja comprar mais um carta?`)
+            pontuacaoUsuario = pontuacaoUsuario
+            if(inicio === false){
+               alert(`Usuário cartas: ${jogadaUsuario.carta1.texto} ${jogadaUsuario.carta2.texto}. Pontuação: ${pontuacaoUsuario}\nComputador cartas: ${jogadaComputador.carta1.texto} ${jogadaComputador.carta2.texto}. Pontuação ${pontuacaoComputador}\n${pontos(pontuacaoUsuario, pontuacaoComputador)}`)
+            }else{
+               inicio = confirm(`Suas cartas são ${jogadaUsuario.carta1.texto} ${jogadaUsuario.carta2.texto} ${jogadaUsuario.carta3.texto}. A carta revelada do computador é ${jogadaComputador.carta1.texto}\nDeseja comprar mais um carta?`)
+               pontuacaoUsuario = pontuacaoUsuario + jogadaUsuario.carta3.valor
+               if(inicio === false){
+                  alert(`Usuário cartas: ${jogadaUsuario.carta1.texto} ${jogadaUsuario.carta2.texto} ${jogadaUsuario.carta3.texto}. Pontuação: ${pontuacaoUsuario}\nComputador cartas: ${jogadaComputador.carta1.texto} ${jogadaComputador.carta2.texto}. Pontuação ${pontuacaoComputador}\n${pontos(pontuacaoUsuario, pontuacaoComputador)}`)
+               }else{
+                  pontuacaoUsuario = pontuacaoUsuario + jogadaUsuario.carta4.valor
+                  alert(`Usuário cartas: ${jogadaUsuario.carta1.texto} ${jogadaUsuario.carta2.texto} ${jogadaUsuario.carta3.texto} ${jogadaUsuario.carta4.texto}. Pontuação: ${pontuacaoUsuario}\nComputador cartas: ${jogadaComputador.carta1.texto} ${jogadaComputador.carta2.texto}. Pontuação ${pontuacaoComputador}\n${pontos(pontuacaoUsuario, pontuacaoComputador)}`)
+                  inicio = false
+               }
+            }
+         }
+      }   
+   }
+}   
 
+
+
+jogando(calculaPontos)
