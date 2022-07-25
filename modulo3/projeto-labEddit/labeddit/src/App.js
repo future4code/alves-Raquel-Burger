8d19b1de273db5a6db60f4dc8e52f052a784f21b
Raquel Baptista Burger <raquelburgerpsi@gmail.com>
2022-07-25 18:46:37 -0300

    rotas e header

--- modulo3/projeto-labEddit/labeddit/src/App.js
+++ modulo3/projeto-labEddit/labeddit/src/App.js
@@ -1,7 +1,3 @@
-import CommentPage from "./pages/CommentPage/CommentPage";
-import FeedPage from "./pages/FeedPage/FeedPage";
-import LoginPage from "./pages/LoginPage/LoginPage";
-import SingUpPage from "./pages/SingUpPage/SingUpPage";
 import Router from "./routes/Router";
 
 function App() {
--- modulo3/projeto-labEddit/labeddit/src/components/Header/Header.js
+++ modulo3/projeto-labEddit/labeddit/src/components/Header/Header.js
@@ -1,9 +1,48 @@
 import React from 'react'
-
+import {useNavigate} from 'react-router-dom'
+import { ButtonClose, ButtonLogin, ContainerHeader, LogoHeader } from './HeaderStyled'
+import Logo from '../../assets/logo.png'
+import { goToLogin, goBack } from '../../routes/coordinator'
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
+   const navigate = useNavigate()
+
+    const showHeader = () => {
+        if (window.location.pathname === "login") {
+            return (
+                <>
+                </>
+            )
+        } else if (window.location.pathname === "/singup") {
+            return (
+                <ContainerHeader>
+                    <LogoHeader src={Logo} />
+                    <ButtonLogin>Entrar</ButtonLogin>
+                </ContainerHeader>
+            )
+        } else if (window.location.pathname === "/") {
+            return (
+                <ContainerHeader>
+                    <LogoHeader src={Logo} />
+                    <ButtonLogin onClick={() => goToLogin(navigate)}>Logout</ButtonLogin>
+                </ContainerHeader>
+            )
+        } else if (window.location.pathname === "/comment") {
+            return (
+                <ContainerHeader>
+                    <ButtonClose onClick={() => goBack(navigate)}> X </ButtonClose>
+                    <LogoHeader src={Logo} />
+                    <ButtonLogin onClick={() => goToLogin(navigate)}>Logout</ButtonLogin>
+                </ContainerHeader>
+            )
+        }
+    }
+
+
+    return (
+        <>
+            {showHeader()}
+        </>
+    )
 }
 
 export default Header
