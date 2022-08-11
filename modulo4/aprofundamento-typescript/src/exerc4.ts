0867fd914c553b884db76dd0b83a0d83b934f6c1
Raquel Baptista Burger <raquelburgerpsi@gmail.com>
2022-08-11 17:14:12 -0300

    exercicios aprofundando Type

---
+++ modulo4/aprofundamento-typescript/src/exerc4.ts
@@ -0,0 +1,27 @@
+type pokemon = {
+    name: string,
+    types: string,
+    healthPoints: number
+}
+
+const pokemon1: pokemon = {
+    name: "Charmander",
+    types: "Fire",
+    healthPoints: 28
+}
+
+const pokemon2: pokemon = {
+    name: "Bulbasaur",
+    types: "Grass/Poison",
+    healthPoints: 31
+}
+
+const pokemon3: pokemon = {
+    name: "Squirtle",
+    types: "Water",
+    healthPoints: 35
+}
+
+//b usaria o comando tsc depois de já ter configurado o tsconfig.json
+//c para isso a confuguração do tsconfig.json
+//d sim, usando o tsc config
